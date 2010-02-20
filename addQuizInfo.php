2ac10bf6f2efbe488a0b392a19db24a0940beeed
Joe Balough <scallopedllama@users.sourceforge.net>
2010-02-20 07:59:50 -0500

    made it so I can pass values in GET to fill in fields (for loading)

--- addQuizInfo.php
+++ addQuizInfo.php
@@ -1,16 +1,26 @@
-<?php include 'util.php'; ?>
+<?php 
+  include 'util.php';
+  //get stuff passed in get or use default empties
+  $title_txt = isset($_GET['title_txt']) ? $_GET['title_txt'] : '';
+  $title_pic_src = isset($_GET['title_pic_src']) ? $_GET['title_pic_src'] : '';
+  $title_pic_alt = isset($_GET['title_pic_alt']) ? $_GET['title_pic_alt'] : '';
+  $desc_txt = isset($_GET['$desc_txt']) ? $_GET['$desc_txt'] : '';
+  $desc_pic_src = isset($_GET['$desc_pic_src']) ? $_GET['$desc_pic_src'] : '';
+  $desc_pic_alt = isset($_GET['$desc_pic_alt']) ? $_GET['$desc_pic_alt'] : '';
+   
+?>
 
 <div class="main" id="quiz_data">
   <?php addHider('quiz_data'); ?>
   <div class="sect_head">Quiz <span id="quiz_data_hval">Data</span></div>
   <div class="sect">
     <div class="sub_section">
-      <?php addPic('quiz', 'Quiz picture', 'float_right'); ?>
-      <div class="group_title">Title<input type="text" name="data_txt"  id="data_txt" class="hval"></div>
+      <?php addPic('quiz', 'Quiz picture', $title_pic_src, $title_pic_alt, 'float_right'); ?>
+      <div class="group_title">Title<input type="text" name="data_txt"  id="data_txt" class="hval" value="<?php echo $title_txt; ?>"></div>
     </div>
     <div class="sub_section">
-      <?php addPic('quiz_desc', 'Description picture', 'float_right'); ?>
-      <p class="group_title">Description</p><textarea rows="3" cols="82" name="data_desc" id="data_desc"></textarea>
+      <?php addPic('quiz_desc', 'Description picture', $desc_pic_src, $desc_pic_alt, 'float_right'); ?>
+      <p class="group_title">Description</p><textarea rows="3" cols="82" name="data_desc" id="data_desc"><?php echo $desc_txt; ?></textarea>
     </div>
   </div>
 </div>
--- util.php
+++ util.php
@@ -1,12 +1,12 @@
 <?php
-  function addPic($name, $title_text, $wrapper_class = '') {
+  function addPic($name, $title_text, $src = '', $alt = '', $wrapper_class = '') {
     $class = ' class="pic ' . $wrapper_class . '"';
     $title = empty($title_text) ? '' : '<p class="group_title">' . $title_text . '</p>';
 ?>
     <div<?php echo $class; ?>>
       <?php echo $title; ?>
-      <p>Src: <input type="text" name="data_<?php echo $name; ?>_src" id="data_<?php echo $name; ?>_src"></p> 
-      <p>Alt: <input type="text" name="data_<?php echo $name; ?>_alt" id="data_<?php echo $name; ?>_alt"></p>
+      <p>Src: <input type="text" name="data_<?php echo $name; ?>_src" id="data_<?php echo $name; ?>_src" value="<?php echo $src; ?>"></p> 
+      <p>Alt: &nbsp;<input type="text" name="data_<?php echo $name; ?>_alt" id="data_<?php echo $name; ?>_alt" value="<?php echo $src; ?>"></p>
     </div>
 <?php
   }
